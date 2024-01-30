cba268d9198c5251800a0ad68090700f3c0917f9
hjpark <hj.park@codexbridge.com>
2024-01-30 14:15:55 +0900

    git init

---
+++ sample/src/main/java/com/parksence/sample/domain/UserRole.java
@@ -0,0 +1,5 @@
+package com.parksence.sample.domain;
+
+public enum UserRole {
+    USER, ADMIN;
+}
---
+++ sample/src/main/java/com/parksence/sample/domain/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.parksence.sample.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+
+    private String loginId;
+    private String password;
+
+}
